c11a75ec62bbec35c7cd30b2f7b9c79dd363d342
Syed <salizahid75@gmail.com>
2022-06-01 18:47:18 +0500

    Style: Indentation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -6,18 +5,20 @@ const app = express();
 // Cross Platform Compatibility
 // app.use(cors());
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 // body-parser
 app.use(express.json());
 // setting routes
 app.use('/', require('./routers/index'));
 // Configure Dotenv File
-dotenv.config({ path: '.env' });
+dotenv.config({path: '.env'});
 const PORT = process.env.PORT || 3001;
 
 // Server listing
-app.listen(PORT, () => console.log(`Server is running on http://127.0.0.1:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running on http://127.0.0.1:${PORT}`)
+);
--- models/index.js
+++ models/index.js
@@ -2,17 +2,8 @@ const mongoose = require('mongoose');
 const UserModel = require('./Users');
 const VendorModel = require('./Users');
 mongoose.connect(`mongodb://localhost:27017`);
-<<<<<<< HEAD
 
 module.exports = {
   VendorModel,
   UserModel,
 };
-=======
-
-
-module.exports = {
-    VendorModel,
-    UserModel
-}
->>>>>>> 97837629df9eb1bbe299400bd6308b3c7fd555bb
